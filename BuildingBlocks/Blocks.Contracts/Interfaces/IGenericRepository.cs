479c3d0615f75c8ff5ab3f0a7e72c63ccbca77ef
Hazem Mofdi <hazemmofdi11@gmail.com>
2026-08-04 22:01:23 +0300

    Add generic repository pattern for data access
    Introduced IGenericRepository<T> interface for common CRUD operations on BaseEntity-derived entities. Implemented GenericRepository<T> using EF Core and FlowersAuthDbContext, providing async methods for entity retrieval, addition, update, and deletion.

---
+++ BuildingBlocks/Blocks.Contracts/Interfaces/IGenericRepository.cs
@@ -0,0 +1,18 @@
+﻿using Blocks.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace Blocks.Contracts.Interfaces
+{
+    public interface IGenericRepository<T> where T : BaseEntity
+    {
+        Task<T?> GetByIdAsync(Guid id);
+        Task<IReadOnlyList<T>> GetAllAsync();
+        Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate);
+        Task AddAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
