a56d2858633a3bd00547ea1e498048ef234619be
Hazem Mofdi <hazemmofdi11@gmail.com>
2026-08-04 22:00:48 +0300

    Add EF Core configs and FlowersAuthDbContext setup
    Added configuration classes for Customer, Driver, and User entities to define relationships and unique constraints. Introduced FlowersAuthDbContext with DbSet properties and applied all configurations from the assembly.

---
+++ Services/Identity/Identity.Infrastructure/Configurations/CustomerConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Identity.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Identity.Infrastructure.Configurations
+{
+    public class CustomerConfiguration : IEntityTypeConfiguration<Customer>
+    {
+        public void Configure(EntityTypeBuilder<Customer> builder)
+        {
+            builder.HasOne(c => c.User)
+                .WithOne()
+                .HasForeignKey<Customer>(c => c.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
---
+++ Services/Identity/Identity.Infrastructure/Data/FlowersAuthDbContext.cs
@@ -0,0 +1,29 @@
+﻿using Identity.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+
+namespace Identity.Infrastructure.Data
+{
+    public class FlowersAuthDbContext : DbContext
+    {
+
+
+        public FlowersAuthDbContext(DbContextOptions<FlowersAuthDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users => Set<User>();
+        public DbSet<Customer> Customers => Set<Customer>();
+        public DbSet<Driver> Drivers => Set<Driver>();
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
