e77b153c936137f014d011f2812f9f8c52a0e4e4
MahmoudAmin5 <ma7moud888.amim@gmail.com>
2026-08-13 13:20:07 +0300

    feat(catalog): implement Unit of Work and Generic Repository pattern

---
+++ Services/Catalog/Catalog Service/Persistence/Repositories/GenericRepository.cs
@@ -0,0 +1,105 @@
+using Blocks.Contracts.Interfaces;
+using Blocks.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Catalog_Service.Persistence.Repositories;
+
+public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
+{
+    protected readonly FlowersCatalogDbContext _context;
+    protected readonly DbSet<T> _dbSet;
+
+    public GenericRepository(FlowersCatalogDbContext context)
+    {
+        _context = context;
+        _dbSet = context.Set<T>();
+    }
+
+    public IQueryable<T> GetQueryable()
+    {
+        return _dbSet.AsQueryable();
+    }
+
+    public async Task<T?> GetByIdAsync(Guid id)
+    {
+        return await _dbSet.FindAsync(id);
+    }
+
+    public async Task<IReadOnlyList<T>> GetAllAsync()
+    {
+        return await _dbSet.ToListAsync();
+    }
+
+    public async Task<IReadOnlyList<T>> FindAsync(Expression<Func<T, bool>> predicate)
+    {
+        return await _dbSet.Where(predicate).ToListAsync();
+    }
+
+    public async Task<TResult?> FirstOrDefaultAsync<TResult>(
+        Expression<Func<T, bool>> predicate,
+        Expression<Func<T, TResult>> selector)
+    {
+        return await _dbSet.Where(predicate).Select(selector).FirstOrDefaultAsync();
+    }
+
+    public async Task AddAsync(T entity)
+    {
+        await _dbSet.AddAsync(entity);
+    }
+
+    public void Update(T entity)
+    {
+        _dbSet.Update(entity);
+    }
+
+    public void UpdatePartial(T entity, params Expression<Func<T, object>>[] updatedProperties)
+    {
+        _context.Attach(entity);
+
+        var entry = _context.Entry(entity);
+        foreach (var property in updatedProperties)
+        {
+            entry.Property(property).IsModified = true;
+        }
+    }
+
+    public void SaveInclude(T entity, params string[] includedProperties)
+    {
+        var localEntity = _dbSet.Local.FirstOrDefault(e => ((dynamic)e).Id == ((dynamic)entity).Id);
+        Microsoft.EntityFrameworkCore.ChangeTracking.EntityEntry entry;
+
+        if (localEntity == null)
+        {
+            entry = _context.Entry(entity);
+        }
+        else
+        {
+            entry = _context.ChangeTracker.Entries<T>().First(e => ((dynamic)e.Entity).Id == ((dynamic)entity).Id);
+        }
+
+        foreach (var property in entry.Properties)
+        {
+            if (property.Metadata.IsPrimaryKey())
+            {
+                continue;
+            }
+            else
+            {
+                if (includedProperties.Contains(property.Metadata.Name))
+                {
+                    property.IsModified = true;
+                }
+                else
+                {
+                    property.IsModified = false;
+                }
+            }
+        }
+    }
+
+    public void Delete(T entity)
+    {
+        _dbSet.Remove(entity);
+    }
+}
