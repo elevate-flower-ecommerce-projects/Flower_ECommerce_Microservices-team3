bc2f1312d068d55b3ccc72596f736a29561b9b57
Hazem Mofdi <hazemmofdi11@gmail.com>
2026-08-15 14:40:08 +0300

    Add ImageUrl column to Occasions table
    Added non-nullable ImageUrl (nvarchar(500)) column with default empty string to catalog.Occasions. Updated EF Core migration, model snapshot, and designer files accordingly.

--- Services/Catalog/Catalog Service/Migrations/20260815094850_AddImageUrlToOccasion.Designer.cs
+++ Services/Catalog/Catalog Service/Migrations/20260815094850_AddImageUrlToOccasion.Designer.cs
@@ -4,6 +4,7 @@ using Catalog_Service.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Catalog_Service.Migrations
 {
     [DbContext(typeof(FlowersCatalogDbContext))]
-    partial class FlowersCatalogDbContextModelSnapshot : ModelSnapshot
+    [Migration("20260815094850_AddImageUrlToOccasion")]
+    partial class AddImageUrlToOccasion
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
