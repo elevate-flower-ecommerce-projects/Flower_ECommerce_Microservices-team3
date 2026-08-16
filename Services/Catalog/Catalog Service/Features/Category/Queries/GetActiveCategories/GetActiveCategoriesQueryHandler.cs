841b0c2e28615fee1b8d5ab993d927f56f4f0f79
Hadeer Mohammed <hadeer@example.com>
2026-08-16 19:05:48 +0300

    Add get products by category

---
+++ Services/Catalog/Catalog Service/Features/Category/Queries/GetActiveCategories/GetActiveCategoriesQueryHandler.cs
@@ -0,0 +1,34 @@
+﻿using Catalog_Service.Persistence;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+
+namespace Catalog_Service.Features.Categories.Queries.GetActiveCategories;
+
+public class GetActiveCategoriesQueryHandler
+    : IRequestHandler<GetActiveCategoriesQuery, List<CategoryDto>>
+{
+    private readonly FlowersCatalogDbContext _context;
+
+    public GetActiveCategoriesQueryHandler(
+        FlowersCatalogDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<CategoryDto>> Handle(
+        GetActiveCategoriesQuery request,
+        CancellationToken cancellationToken)
+    {
+        return await _context.Categories
+            .AsNoTracking()
+            .Where(c => c.IsActive)
+            .OrderBy(c => c.DisplayOrder)
+            .Select(c => new CategoryDto
+            {
+                Id = c.Id,
+                Name = c.Name,
+                Icon = c.Icon
+            })
+            .ToListAsync(cancellationToken);
+    }
+}
---
+++ Services/Catalog/Catalog Service/Features/Products/GetProductByCategory/Querry/GetProductsByCategoryQuery.cs
@@ -0,0 +1,9 @@
+﻿using Blocks.Contracts.Common;
+using MediatR;
+
+namespace Catalog_Service.Features.Products.GetProductByCategory.Querry;
+
+public sealed record GetProductsByCategoryQuery(
+    Guid CategoryId,
+    string? Language = "en"
+) : IRequest<Result<List<ProductDto>>>;
