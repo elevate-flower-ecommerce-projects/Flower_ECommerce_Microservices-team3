1a54a8c1d26fe20b760a1311e819aecb5e39192d
Amr-shawky <amrshawky937@gmail.com>
2026-08-23 21:39:37 +0300

    fix

--- Services/Catalog/Catalog Service/Migrations/20260823123213_SyncCatalogModel.cs
+++ Services/Catalog/Catalog Service/Migrations/20260823123213_SyncCatalogModel.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -10,19 +10,11 @@ namespace Catalog_Service.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.AddColumn<string>(
-                name: "NameAr",
-                table: "Categories",
-                type: "nvarchar(max)",
-                nullable: true);
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropColumn(
-                name: "NameAr",
-                table: "Categories");
         }
     }
 }
